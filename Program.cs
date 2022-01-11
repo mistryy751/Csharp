5ec4ee611878b2b37b9ce8e55104b7c0cb11d453
Yash_Mistry <52452640+mistryy751@users.noreply.github.com>
2022-01-11 11:14:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Csharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+            /* Console.WriteLine("Enter The Number of lines:");
+            int n = Convert.ToInt32(Console.ReadLine());
+             for(int i=0;i<n;i++)
+             {
+                 for (int j = 0; j <= i; j++)
+                 {
+                   Console.Write(i % 2 == 0 ? "*" : "#");    
+                 }
+                  Console.WriteLine("");
+             } */
+
+            // Array.Singlearraytest();
+            //Array.BubbleSort();
+            //Calender.Simple();
+            cal_task2.tempo();
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ cal_task1.cs
@@ -0,0 +1,31 @@
+﻿
+using Csharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class cal_task1
+    {
+
+        public static void Simple()
+        {
+            char[] dys = { 'S', 'M', 'T', 'W', 'T', 'F', 'S' };
+            int[,] da = new int[,] { { 1, 2, 3, 4, 5, 6, 7 }, { 8, 9, 10, 11, 12, 13, 14 }, { 15, 16, 17, 18, 19, 20, 21 }, { 22, 23, 24, 25, 26, 27, 28 },{ 29, 30, 31 ,0,0,0,0} };
+            System.Console.WriteLine(dys+"\n");
+            Console.WriteLine(da);
+            /* for (int i = 0; i <da.Length; i++)
+            {
+                for (int j = 0; j < 7; j++) 
+                {
+                    da[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(da[i,j]);
+                }
+            }*/
+           
+        }
+    }
+}
